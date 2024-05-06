488fe46cfad161cfb08d84864220dab563e83802
nadu <nadu@WINDOWS-UINETAB>
2024-05-06 03:46:49 -0700

    Add project files.

---
+++ labsheet 02 question1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title:{Title}");
+            Console.WriteLine($"Author:{Author}");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            book book = new book();
+            book.Title = "Harry Potter";
+            book.Author = "J.K.Rowling";
+            Console.WriteLine("\nBook Information:");
+            book.DisplayInfo();
+        }
+    }
+}
